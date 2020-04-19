927d4d46799fdb33bbf4d32a56e8a824a5757a21
kerpigli <kerpigli@hotmail.com>
2020-04-19 11:59:48 +0200

    update

--- smple/src/app/profile/profile.component.ts
+++ smple/src/app/profile/profile.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import {  ActivatedRoute } from '@angular/router';
+
+
+import { ProductService } from '../product.service';
+import { Product } from '../entities/product.entity';
+import { Item } from '../entities/item.entity';
 
 @Component({
   selector: 'app-profile',
@@ -9,17 +15,10 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private productService: ProductService, private activatedRoute: ActivatedRoute) { }
 
   public user = {};
   public issues = {};
-
-  ngOnInit() {
-
-    this.authService.getUser().subscribe(data => this.user = data);
-    this.authService.getIssues().subscribe(data => this.issues = data);
-    
-  }
 
   
 
@@ -27,5 +26,87 @@ export class ProfileComponent implements OnInit {
     this.authService.logout();
     this.router.navigateByUrl("/");
   }
+  private items: Item[] = [];
+  private total: number = 0;
+  private itemQuantity: number = 0;
 
-}
+
+  ngOnInit() {
+    this.authService.getUser().subscribe(data => this.user = data);
+    this.authService.getIssues().subscribe(data => this.issues = data);
+    
+
+    this.activatedRoute.params.subscribe(params => {
+      var id = params['id'];
+      if (id) {
+        var item: Item = {
+          product: this.productService.getOneProduct(id),
+          quantity: 1
+        };
+        if (localStorage.getItem('cart') == null) {
+          let cart: any = [];
+          cart.push(JSON.stringify(item));
+          localStorage.setItem('cart', JSON.stringify(cart));
+        } else {
+          let cart: any = JSON.parse(localStorage.getItem('cart'));
+          let index: number = -1;
+          for (var i = 0; i < cart.length; i++) {
+            let item: Item = JSON.parse(cart[i]);
+            if (item.product.id == id) {
+              index = i;
+              break;
+            }
+          }
+          if (index == -1) {
+            cart.push(JSON.stringify(item));
+            localStorage.setItem('cart', JSON.stringify(cart));
+          } else {
+           
+            let item: Item = JSON.parse(cart[index]);
+            item.quantity += 1;
+            cart[index] = JSON.stringify(item);
+            localStorage.setItem("cart", JSON.stringify(cart));
+          }
+        }
+        this.loadCart();
+        
+      } else {
+        this.loadCart();
+        
+      }
+    });
+
+  }
+ 
+  loadCart(): void {
+    this.itemQuantity = 0;
+    this.total = 0;
+    this.items = [];
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    for (var i = 0; i < cart.length; i++) {
+      let item = JSON.parse(cart[i]);
+      this.items.push({
+        product: item.product,
+        quantity: item.quantity
+      });
+      this.total += item.product.price * item.quantity;
+      this.itemQuantity += item.quantity;
+    }
+  }
+
+  remove(id: string): void {
+    let cart: any = JSON.parse(localStorage.getItem('cart'));
+    let index: number = -1;
+    for (var i = 0; i < cart.length; i++) {
+      let item: Item = JSON.parse(cart[i]);
+      if (item.product.id == id) {
+
+        cart.splice(i, 1);
+        break;
+        
+      }
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.loadCart();
+  }
+ }
